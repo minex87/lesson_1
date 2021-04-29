578d64b3210baef3c34f11159049e441f2b5446e
minehiroki <minehiroki@gmail.com>
2021-04-29 09:35:00 +0900

    Initial commit

---
+++ search.py
@@ -0,0 +1,24 @@
+import csv
+
+# 検索ソース
+source = ["ねずこ", "たんじろう", "きょうじゅろう", "ぎゆう", "げんや", "かなお", "ぜんいつ"]
+
+### 検索ツール
+def search():
+    word = input("鬼滅の登場人物の名前を入力してください >>> ")
+
+    for i in range(len(source)):
+        if source[i] == word:
+            print("{}が見つかりました".format(word))
+            break
+        if i == len(source)-1:
+            print(f'{word}は見つかりませんでした。追加登録します。')
+            source.append(word)
+            print(source)
+
+    with open('search.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(source)
+
+if __name__ == "__main__":
+    search()
